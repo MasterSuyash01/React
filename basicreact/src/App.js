8097aa32dceedde1950701b7d0e5a29f9ea0bfbc
Suyash Srivastav <suyashsrivastav1@gmail.com>
2023-08-21 19:19:17 +0530

    Hello Ji

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <h1>Hello, world! I am learning react.</h1>
   );
 }
 
--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Hello there!</h1>
+      <h1>Hello there! I am learning React and this is created using Vite</h1>
     </>
   )
 }
